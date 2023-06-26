51f05f29468512cc0b3406c0ca9ba59f384f1647
Mallta <pggian1985@gmail.com>
2023-06-26 05:26:52 -0300

    add voice comand on inputs

--- buscar.js
+++ buscar.js
@@ -1,60 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-  
-    const searchInput = document.getElementById('search-input');
-    const searchResults = document.getElementById('search-results');
-  
-    searchInput.addEventListener('input', performSearch);
-  
-    function performSearch() {
-      const searchValue = searchInput.value.trim().toLowerCase();
-  
-      if (searchValue === '') {
-        clearResults();
-        return;
-      }
-  
-      fetch('http://localhost:3000/api/v1/customers')
-        .then(response => response.json())
-        .then(data => {
-          if (Array.isArray(data)) {
-            const filteredResults = data.filter(customer => {
-              const customerName = customer.name.toLowerCase();
-              return customerName.startsWith(searchValue);
-            });
-            renderResults(filteredResults);
-          } else {
-            throw new Error(data.error);
-          }
-        })
-        .catch(error => showError(`Ocorreu um erro ao buscar os clientes: ${error.message}`));
-    }
-  
-    function renderResults(results) {
+  const searchInput = document.getElementById('search-input');
+  const searchResults = document.getElementById('search-results');
+
+  searchInput.addEventListener('input', performSearch);
+
+  function performSearch() {
+    const searchValue = searchInput.value.trim().toLowerCase();
+
+    if (searchValue === '') {
       clearResults();
-  
-      if (results.length === 0) {
-        searchResults.innerHTML = '<p>Nenhum cliente encontrado.</p>';
-        return;
-      }
-  
-      const ul = document.createElement('ul');
-  
-      results.forEach(customer => {
-        const li = document.createElement('li');
-        const customerInfo = `${customer.name} ${customer.lastName} - Código: ${customer.customerCode}`;
-        li.textContent = customerInfo;
-        ul.appendChild(li);
-      });
-  
-      searchResults.appendChild(ul);
+      return;
     }
-  
-    function clearResults() {
-      searchResults.innerHTML = '';
+
+    fetch('http://localhost:3000/api/v1/customers')
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          const filteredResults = data.filter(customer => {
+            const customerName = customer.name.toLowerCase();
+            return customerName.startsWith(searchValue);
+          });
+          renderResults(filteredResults);
+        } else {
+          throw new Error(data.error);
+        }
+      })
+      .catch(error =>
+        showError(`Ocorreu um erro ao buscar os clientes: ${error.message}`)
+      );
+  }
+
+  function renderResults(results) {
+    clearResults();
+
+    if (results.length === 0) {
+      searchResults.innerHTML = '<p>Nenhum cliente encontrado.</p>';
+      return;
     }
-  
-    function showError(message) {
-      searchResults.innerHTML = `<p class="error">${message}</p>`;
-    }
-  });
+
+    const ul = document.createElement('ul');
+
+    results.forEach(customer => {
+      const li = document.createElement('li');
+      const customerInfo = `${customer.name} ${customer.lastName} - Código: ${customer.customerCode}`;
+      li.textContent = customerInfo;
+      ul.appendChild(li);
+    });
+
+    searchResults.appendChild(ul);
+    searchResults.classList.add('visible'); // Adiciona a classe 'visible' para tornar os resultados visíveis
+  }
+
+  function clearResults() {
+    searchResults.innerHTML = '';
+    searchResults.classList.remove('visible'); // Remove a classe 'visible' para ocultar os resultados
+  }
+
+  function showError(message) {
+    searchResults.innerHTML = `<p class="error">${message}</p>`;
+    searchResults.classList.remove('visible'); // Remove a classe 'visible' para ocultar os resultados
+  }
+});
--- script.js
+++ script.js
@@ -1,40 +1,111 @@
 function submitForm(event) {
-  
-    event.preventDefault();
-  
-    const form = document.getElementById('customer-form');
-    const formData = new FormData(form);
-  
-    const payload = {
-      name: formData.get('name'),
-      lastName: formData.get('lastName'),
-      city: formData.get('city'),
-      state: formData.get('state'),
-      address: formData.get('address'),
-      customerCode: formData.get('customerCode')
-      
-    };
-  
-    fetch('http://localhost:3000/api/v1/customers', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(response => {
-        if (response.ok) {
-          document.getElementById('message').textContent = 'Dados enviados com sucesso!';
-          form.reset();
-        } else {
-          throw new Error('Ocorreu um erro ao enviar os dados.');
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        document.getElementById('message').textContent = 'Ocorreu um erro ao enviar os dados.';
-      });
-  }
-  
+  event.preventDefault();
+
   const form = document.getElementById('customer-form');
-  form.addEventListener('submit', submitForm);
+  const formData = new FormData(form);
+
+  const payload = {
+    name: formData.get('name').toLowerCase(),
+    lastName: formData.get('lastName').toLowerCase(),
+    city: formData.get('city').toLowerCase(),
+    state: formData.get('state').toLowerCase(),
+    address: formData.get('address').toLowerCase(),
+    customerCode: formData.get('customerCode').toLowerCase()
+  };
+
+  fetch('http://localhost:3000/api/v1/customers', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+    .then(response => {
+      if (response.ok) {
+        document.getElementById('message').textContent = 'Obrigado por colaborar!';
+        form.reset();
+      } else {
+        throw new Error('Ocorreu um erro ao enviar os dados.');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      document.getElementById('message').textContent = 'Ocorreu um erro ao enviar os dados.';
+    });
+}
+
+const form = document.getElementById('customer-form');
+form.addEventListener('submit', submitForm);
+
+const customerCodeInput = document.getElementById('customerCode');
+
+customerCodeInput.addEventListener('input', () => {
+  const value = customerCodeInput.value;
+  if (value.length > 4) {
+    customerCodeInput.value = value.slice(0, 4);
+  }
+});
+
+customerCodeInput.addEventListener('input', () => {
+  const value = customerCodeInput.value;
+  customerCodeInput.value = value.replace(/\D/g, '');
+});
+
+// Captura de voz
+const voiceButton = document.getElementById('voiceButton');
+
+if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  let activeField = null;
+
+  recognition.lang = 'pt-BR';
+  recognition.continuous = false;
+  recognition.interimResults = false;
+
+  voiceButton.addEventListener('mousedown', () => {
+    recognition.start();
+    voiceButton.classList.add('active');
+  });
+
+  voiceButton.addEventListener('mouseup', () => {
+    recognition.stop();
+    voiceButton.classList.remove('active');
+  });
+
+  recognition.addEventListener('result', (event) => {
+    const voiceResult = event.results[0][0].transcript.trim();
+    
+    if (activeField) {
+      activeField.value = voiceResult;
+      activeField = null;
+    }
+  });
+
+  recognition.addEventListener('error', (event) => {
+    console.error('Erro de reconhecimento de voz:', event.error);
+  });
+
+  // Função para iniciar o reconhecimento de voz no campo específico
+  function startRecognition(fieldName) {
+    
+    activeField = form.querySelector(`input[name="${fieldName}"], select[name="${fieldName}"]`);
+    if (activeField) {
+      recognition.start();
+      voiceButton.classList.add('active');
+    }
+  }
+
+  // Adicionar o evento de clique do ícone de microfone a cada campo
+  const voiceIcons = document.querySelectorAll('.voice-icon');
+  voiceIcons.forEach(icon => {
+    const fieldName = icon.getAttribute('data-field');
+    icon.addEventListener('click', () => {
+      startRecognition(fieldName);
+    });
+  });
+} else {
+  voiceButton.disabled = true;
+  console.error('A API SpeechRecognition não é suportada neste navegador.');
+}
+
+
