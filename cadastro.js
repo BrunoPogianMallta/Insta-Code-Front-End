caa4a794787ea87b0aa51525d17c38ed5a4ec526
Mallta <pggian1985@gmail.com>
2023-06-29 19:46:04 -0300

    fix

--- cadastro.js
+++ cadastro.js
@@ -40,29 +40,32 @@ document.addEventListener('DOMContentLoaded', function() {
         firstName: firstName,
         lastName: lastName,
         email: email,
-        password: password
+        password: password,
+        acceptedTerms: true
       };
   
-      // Enviar a requisição para criar o usuário no backend
+      // Enviar a requisição para criar o usuário no banco de dados
       fetch('https://instacodehelper.cyclic.app/api/v1/user', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(user),
-        mode: 'cors'
+        mode: 'cors' // Certifique-se de que essa opção esteja definida como 'cors'
       })
         .then(function(response) {
           if (response.ok) {
-            // Redirecionar para a página de termos após o cadastro bem-sucedido
-            window.location.href = 'termo.html';
+            // Redirecionar para a página do dashboard após o cadastro bem-sucedido
+            window.location.href = 'dashboard.html';
           } else {
             throw new Error('Ocorreu um erro ao criar o usuário.');
           }
         })
         .catch(function(error) {
           console.log(error);
+          // Exibir mensagem de erro e redirecionar para a página de index
           alert('Ocorreu um erro ao criar o usuário. Por favor, tente novamente mais tarde.');
+          window.location.href = 'index.html';
         });
     });
   });
