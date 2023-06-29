3643092f22cf5013bac2a4be9503fdf578a2bed3
Mallta <pggian1985@gmail.com>
2023-06-29 19:54:58 -0300

    fixx

--- cadastro.js
+++ cadastro.js
@@ -44,28 +44,41 @@ document.addEventListener('DOMContentLoaded', function() {
         acceptedTerms: true
       };
   
-      // Enviar a requisição para criar o usuário no banco de dados
-      fetch('https://instacodehelper.cyclic.app/api/v1/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user),
-        mode: 'cors' // Certifique-se de que essa opção esteja definida como 'cors'
-      })
-        .then(function(response) {
-          if (response.ok) {
-            // Redirecionar para a página do dashboard após o cadastro bem-sucedido
-            window.location.href = 'dashboard.html';
-          } else {
-            throw new Error('Ocorreu um erro ao criar o usuário.');
-          }
-        })
-        .catch(function(error) {
-          console.log(error);
-          // Exibir mensagem de erro e redirecionar para a página de index
-          alert('Ocorreu um erro ao criar o usuário. Por favor, tente novamente mais tarde.');
-          window.location.href = 'index.html';
-        });
+      // Abrir a página 'termo.html'
+      const termoWindow = window.open('termo.html', '_blank');
+  
+      // Aguardar o usuário aceitar ou recusar os termos
+      termoWindow.addEventListener('message', function(event) {
+        if (event.data === 'accepted') {
+          // Enviar a requisição para criar o usuário no banco de dados
+          fetch('https://instacodehelper.cyclic.app/api/v1/user', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user),
+            mode: 'cors'
+          })
+            .then(function(response) {
+              if (response.ok) {
+                // Redirecionar para a página 'index.html' após o cadastro bem-sucedido
+                window.location.href = 'index.html';
+              } else {
+                throw new Error('Ocorreu um erro ao criar o usuário.');
+              }
+            })
+            .catch(function(error) {
+              console.log(error);
+              // Exibir mensagem de erro e redirecionar para a página 'index.html'
+              alert('Ocorreu um erro ao criar o usuário. Por favor, tente novamente mais tarde.');
+              window.location.href = 'index.html';
+            });
+        } else if (event.data === 'declined') {
+          // Exibir mensagem de termos não aceitos
+          alert('Os termos não foram aceitos. O cadastro foi cancelado.');
+          // Fechar a janela ou guia do termo
+          termoWindow.close();
+        }
+      });
     });
   });
